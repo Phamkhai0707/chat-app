522bf9ffdc7ccae042a4b537362ed8013d368f2e
Khai <phamkhai070720@gmail.com>
2024-10-07 13:52:53 +0700

    fix: image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,10 +9,9 @@ const font = Poppins({
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center bg-cover">
-    {/* <div className="h-full flex items-center justify-center bg-[url('/assets/background.jpg')] bg-cover"> */}
+    <div className="h-full flex items-center justify-center bg-[url('/assets/background.jpg')] bg-cover">
       <div className="absolute flex flex-row top-10 left-10 gap-x-2 items-center">
-        {/* <Image src={require("@/public/assets/logo.png")} alt="logo" width={50} height={50}/> */}
+        <Image src={require("@/public/assets/logo.png")} alt="logo" width={50} height={50}/>
         <span className={cn(
           "text-3xl font-semibold bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-400 to-indigo-500 bg-clip-text text-transparent",
           font.className
